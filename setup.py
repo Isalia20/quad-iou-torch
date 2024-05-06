efda546e571966be1d6ee228cd20de157198da48
isalia20 <irakli.salia854@gmail.com>
2024-05-06 20:03:59 +0400

    files in src and build from src

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 from torch.utils import cpp_extension
 
 setup(name="quad_iou",
-      ext_modules=[cpp_extension.CUDAExtension('quad_iou', ['quad_iou_tensors_cuda.cpp', 'quad_iou_tensors.cu'])],
+      ext_modules=[cpp_extension.CUDAExtension('quad_iou', ['src/quad_iou_tensors_cuda.cpp', 'src/quad_iou_tensors.cu'])],
       cmdclass={'build_ext': cpp_extension.BuildExtension}
       )
