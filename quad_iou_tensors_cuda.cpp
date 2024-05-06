e2e78aefcd64fff3ebe746a1a8f07bbfe4711a5e
isalia20 <irakli.salia854@gmail.com>
2024-05-06 18:55:41 +0400

    all checks in checks.cuh

--- quad_iou_tensors_cuda.cpp
+++ quad_iou_tensors_cuda.cpp
@@ -3,12 +3,8 @@
 #include <torch/torch.h>
 #include <iostream>
 
-#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor");
-
 torch::Tensor calculateIoUCudaTorch(torch::Tensor quad_0, torch::Tensor quad_1);
 torch::Tensor calculateIoU(torch::Tensor quads_0, torch::Tensor quads_1){
-    CHECK_CUDA(quads_0);
-    CHECK_CUDA(quads_1);
     return calculateIoUCudaTorch(quads_0, quads_1);
 }
 
