4af29322e563fb9c1c99021b2d86d8fc8ee6616d
isalia20 <irakli.salia854@gmail.com>
2024-05-13 20:22:59 +0400

    more removal of functions

--- src/quad_iou_tensors_cuda.cpp
+++ src/quad_iou_tensors_cuda.cpp
@@ -4,12 +4,9 @@
 #include <iostream>
 
 torch::Tensor calculateIoUCudaTorch(torch::Tensor quad_0, torch::Tensor quad_1);
-torch::Tensor calculateIoU(torch::Tensor quads_0, torch::Tensor quads_1){
-    return calculateIoUCudaTorch(quads_0, quads_1);
-}
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("calculateIoU", &calculateIoU, "IoU Calculation for Quads (CUDA)");
+    m.def("calculateIoU", &calculateIoUCudaTorch, "IoU Calculation for Quads (CUDA)");
 };
 
 #endif
